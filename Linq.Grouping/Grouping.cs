d92cf1fa30c64936e5d9d7d1a584c1d88b7affda
Muxammad <muxammadcodes@gmail.com>
2023-02-19 20:30:53 +0500

    groupby

--- Linq.Grouping/Grouping.cs
+++ Linq.Grouping/Grouping.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Student> students = new List<Student>()
+            {
+                new Student() {Id = 1,Name = "Muxammad",Age = 20},
+                new Student() {Id = 2,Name = "Rasulbek", Age = 19},
+                new Student() {Id = 3,Name = "Ali", Age = 18},
+                new Student() {Id = 4,Name = "javohir", Age = 19},
+                new Student() {Id = 5,Name = "Jamshid", Age=20},
+                new Student() {Id = 6,Name = "Muhammad", Age = 18}
+            };
+
+            var groupbyResult = students.GroupBy(x => x.Age);
+
+            foreach (var groupby in groupbyResult)
+            {
+                Console.WriteLine($"Age Group: {groupby.Key}");
+                foreach (var s in groupby)
+                {
+                    Console.WriteLine($"Student name {s.Name}");
+                }
+            }
+
+            //Grouping with names
+            var groupBy= students.GroupBy(x => x.Name);
+
+            foreach (var grouped in groupBy)
+            {
+                Console.WriteLine($"By Name: {grouped.Key}");
+                foreach (var gr in grouped)
+                {
+                    Console.WriteLine($"{gr.Id} {gr.Name} {gr.Age} ");
+                }
+            }
+
         }
     }
 }
---
+++ Linq.Grouping/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Linq.Grouping
+{
+    public class Student
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+    }
+}
