c35815c6344c0587fbb06e6df79120440e42a9f8
Muxammad <muxammadcodes@gmail.com>
2023-02-19 15:44:36 +0500

    secondCommit

--- Linq.Filtering/Filtering.cs
+++ Linq.Filtering/Filtering.cs
@@ -10,6 +10,25 @@
             {
                 Console.WriteLine(name);
             }
+
+            IEnumerable<string> people = new List<string>() { "Bill", "Stave", "George", "Linux", "John" };
+            var selectedNames = from n in people
+                                where n.Contains("i") && n.Length > 3
+                                select n;
+            foreach (var name in selectedNames)
+            {
+                Console.WriteLine(name);
+            }
+
+            IEnumerable<string> guys = new List<string>() { "Bill", "Stave", "George", "Linux", "John" };
+            var mixSelectedNames = guys.Select(n => n)
+                .Where((n)=> n.Contains("i") && n.Length > 3);   
+            foreach (var name in mixSelectedNames)
+            {
+                Console.WriteLine(name);
+            }
+                                   
+
         }
     }
 }
